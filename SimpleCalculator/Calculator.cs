c8a078e0ce76717277e2340eacc531cb5b7f13fe
Sanchayeta <sanchayeta.ghosh97@gmail.com>
2023-04-03 22:08:56 +0530

    First working calculator application.

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal static class Calculator
+    {
+        public static int Addition(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Division(int x, int y)
+        {
+            return x / y;
+        }
+        public static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,7 +10,39 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            Console.WriteLine("Welcome to the Calculator !");
+            Console.WriteLine("Please provide two numbers to continue :");
+
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please provide the operator to procceed :\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication");
+
+            string operatorAction = Console.ReadLine();
+
+            int result = 0;
+
+            
+            switch (operatorAction)
+            {
+                case "Addition":
+                    result = Calculator.Addition(input1, input2);
+                    break;
+                case "Subtraction":
+                    result = Calculator.Subtraction(input1, input2);
+                    break;
+                case "Division":
+                    result = Calculator.Division(input1, input2);
+                    break;
+                case "Multiplication":
+                    result = Calculator.Multiplication(input1, input2);
+                    break;
+                default:
+                    Console.WriteLine("Kindly insert a valid input");
+                    break;
+            }
+
+            Console.WriteLine("The " + operatorAction + " of " + input1 + " and " + input2 + " is = " + result);
         }
     }
 }
